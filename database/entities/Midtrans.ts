d96a5e863d38e97c1160b7b88ad00d1685da2c7f
muharsadika <id.muharsadika@gmail.com>
2024-05-02 16:05:13 +0700

    refactor: deleting generated primary key id which useless

--- database/entities/Midtrans.ts
+++ database/entities/Midtrans.ts
@@ -1,16 +1,13 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, ManyToOne, OneToMany } from "typeorm"
+import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn, ManyToOne, OneToMany } from "typeorm"
 import { Cart } from "./CartEntity"
 
 @Entity({ name: "midtrans" })
 export class MidtransEntity {
-  @PrimaryGeneratedColumn()
-  id: number
+  @PrimaryColumn()
+  order_id: string
 
   @Column({ nullable: true })
   id_buyer: string
-
-  @Column({ nullable: true })
-  order_id: string
 
   @Column({ nullable: true })
   transaction_status: string
